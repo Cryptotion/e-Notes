0cc7fe05c1dc3e2b0667fe1fdd06abcec04a5a55
Krishna Pratap Kushwah <kushwahkrishna755@gmail.com>
2021-10-28 10:14:14 +0530

    complete

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
+  const host = "https://e-notes-production.up.railway.app/"
   const notesInitial = []
     
   const [notes, setNotes] = useState(notesInitial)
