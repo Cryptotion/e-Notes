a1f7da92f92628322ce9b32910fc2c0b11997374
Krishna Pratap Kushwah <kushwahkrishna755@gmail.com>
2021-11-07 19:32:35 +0530

    tttt

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("https://react-auth-app.up.railway.app/api/auth/login", {
+        const response = await fetch("https://reactauth.herokuapp.com/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
